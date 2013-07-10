a9dafa7bb404a619a44827e9cb14197d95d7866f
Dejan Arbutina <darbutina@gmail.com>
2013-07-10 22:00:56 +0200

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var http = require('http');
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World from index.html!');
+  response.writeHead(200, {'Content-Type': 'text/html'});
+	response.write(fs.readFileSync("./index.html", "utf8").toString());
+	response.end();
 });
 
-var port = process.env.PORT || 5000;
+
+var port = process.env.PORT || 8000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
